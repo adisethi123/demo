0149985eb950bb1f2d5f8a9121e51bc73076b769
Aditya Sethi <aditya.sethi@91springboard.com>
2020-06-01 18:27:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test1';
+
+
+export class AppComponent implements OnInit {
+
+  result : object;
+
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  ngOnInit(){
+    this.http.get("http://api.dev.marche.dailyobjects.com/cp",
+    {
+      headers : new HttpHeaders({'Authorization' : 'Bearer let_me_through_coz_i_got_rights' })
+    })
+    .subscribe((res:any) =>{
+      this.result = res.data.feed
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
